6f8ef10fcca1751fd340062ca7b9508acd50a8bf
Bishnu Mishra <mishravishnu369@gmail.com>
2024-09-05 16:40:16 +0530

    java collection framework

---
+++ JavaCollectionFramework/B_List/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // List interface
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(10);
+        System.out.println("All good");
+    }
+}
